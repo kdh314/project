2cd17c6b40d131ac48e8daf4ff0970aead00c8b3
Seongbeom Kang <sb.kang@outlook.com>
2019-11-30 22:31:09 +0900

    [Android] - FTP 로부터 파일리스트 얻어옴

--- android_crush_box/app/src/main/java/com/dhkang/crushbox/MainActivity.java
+++ android_crush_box/app/src/main/java/com/dhkang/crushbox/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //데이터를 저장하게 되는 리스트
-        List<String> list = new ArrayList<>();
+        final List<String> list = new ArrayList<>();
 
         //리스트뷰와 리스트를 연결하기 위해 사용되는 어댑터
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
@@ -38,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
         //리스트뷰의 어댑터를 지정해준다.
         listview.setAdapter(adapter);
 
+        Thread ftp_thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // FTP 예
+                FTPManager manager = new FTPManager();
+                manager.connect("sbkang.synology.me", "kdh314", "1q2w3e", 21);
+
+                String curr_dir = manager.get_current_directory();
+
+                manager.change_directory("/kdh314");
+
+                String[] file_list = manager.get_file_list(".");
+
+                for(String file : file_list) {
+                    list.add(file);
+
+                }
+            }
+        });
+
+        ftp_thread.start();
 
         //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView,
                                     View view, int position, long id) {
 
+
                 //클릭한 아이템의 문자열을 가져옴
                 String selected_item = (String)adapterView.getItemAtPosition(position);
 
@@ -53,13 +75,5 @@ public class MainActivity extends AppCompatActivity {
                 selected_item_textview.setText(selected_item);
             }
         });
-
-
-        //리스트뷰에 보여질 아이템을 추가
-        list.add("사과");
-        list.add("배");
-        list.add("귤");
-        list.add("바나나");
-
     }
 }
